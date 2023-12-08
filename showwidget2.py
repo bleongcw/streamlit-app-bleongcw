46149b3d0553f66af0052ee27b884e5323cd582b
Bernard Leong <bleongcw@gmail.com>
2023-12-08 13:46:41 +0800

    Add files via upload
    Add showwidget2.py

---
+++ showwidget2.py
@@ -0,0 +1,19 @@
+# Core Packages
+import streamlit as st
+
+#Select/Multiple Select
+my_lang = ["Python", "Julia", "Go", "Rust"]
+
+choice = st.selectbox("Language", my_lang)
+st.write("You selected {}".format(choice))
+
+# Multiple Selection
+spoken_lang = ("English", "French", "Spanish", "Twi")
+my_spoken_lang = st.multiselect("Spoken Lang", spoken_lang, default="English")
+
+# Slider
+# Numbers (Int/Float/Dates)
+age = st.slider("Age", 1,100, 5)
+
+# Any datatype
+color = st.select_slider("Choose Color", options =["yellow", "red", "blue", "black", "white"], value=("yellow", "red"))
